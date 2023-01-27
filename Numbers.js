cdaf45c96af1ddcdf3267e517c04dea8e540a988
Rithik7799 <7799268565.ganga@gmail.com>
2023-01-27 11:22:53 +0530

    i done 30 percent work

---
+++ Numbers.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+
+var num = prompt("enter the number :");
+//console.log(num);
+for(var i=1;i<=num;i++)
+{
+    console.log(i);
+}
---
+++ college.js
@@ -0,0 +1,5 @@
+const prompt = require('prompt-sync')();
+
+/*var fname = prompt("enter the first name : ");
+var lname = prompt("enter the last name : ");
+console.log("your full name is",fname,lname);*/
